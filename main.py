47e83939f9dfa409902f5f91f22ea7da0c275ac0
coding-gene <andricivan86@gmail.com>
2022-01-02 22:36:14 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # todo: https://www.kaggle.com/ronitf/heart-disease-uci
 # todo: https://towardsdatascience.com/predicting-presence-of-heart-diseases-using-machine-learning-36f00f3edb2c
+# todo: https://www.youtube.com/watch?v=1a7bB1ZcZ3k
 from pyspark.sql import SparkSession
 import logging
 import time
@@ -14,12 +15,19 @@ try:
                         datefmt='%Y-%m-%d %H:%M:%S')
     logging.info(f'Pocetak izvrsavanja zadatka.')
 
-    spark = SparkSession.builder.master("local[1]").appName("HeartDisease").getOrCreate()
-    df = spark.read.csv('dataset.csv', header=True)
+    spark = SparkSession.builder.appName("HeartDisease").getOrCreate()
+    df = spark.read.csv(r'dataset.csv', inferSchema=True, header=True)
+    #df.write.parquet(r'dataset.parquet')
+    #df = spark.read.parquet(r'C:\Users\Ivana\PycharmProjects\HeartDiseasePySpark\dataset.parquet')
+    # Split dataset
+    train, test = df.randomSplit([0.7, 0.3], seed=7)
+
+    print(f'Train: {train.count()}')
+    print(f'Test: {test.count()}')
 
     df.printSchema()
     df.show(20)
-    print(df.dtypes)
+    #print(df.dtypes)
 
 except Exception:
     logging.exception(f'Dogodila se greska sljedeceg sadrzaja:')
